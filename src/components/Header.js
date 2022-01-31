048e737e0111aefd9f5eb78f5dddcad9619e0fea
Brandyn <stateofw@gmail.com>
2022-01-31 17:37:51 -0500

    header styled with flexbox

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,47 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from "styled-components";
+
+const StyledHeader = styled.div`
+  .header-container {
+    height: 5vh;
+    width: 90%;
+    margin: auto;
+    min-height: 10vh;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    font-size: 0.8rem;
+  }
+  .title-container {
+    display: flex;
+  }
+  nav {
+    padding-right: 15px;
+  }
+  a {
+    padding: 2px;
+  }
+  button {
+    background-color: #000;
+    color: #ffffff;
+    border: 1px solid rgb(0, 0, 0);
+    border-radius: 3px;
+    height: 2em;
+    padding-left: 24px;
+    padding-right: 24px;
+  }
+  button:hover {
+    background-color: rgb(24, 191, 85);
+    border: 1px solid rgb(24, 191, 85);
+    color: white;
+  }
+`;
 
 const Header = () => {
     return (
+      <StyledHeader>
         <div className="header-container">
           <div className="title-container">
             <h1 className="title">African Marketplace</h1>
@@ -20,7 +59,9 @@ const Header = () => {
               <button id="signup">SIGN UP</button>
             </Link>
           </nav>
-        </div>
+          </div>
+        
+        </StyledHeader>
     )
 }
 
