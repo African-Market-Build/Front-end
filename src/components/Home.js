58fff03f52fdae18b8604200d89a90bcfcb3efc2
Brandyn <stateofw@gmail.com>
2022-02-01 20:38:50 -0500

    home

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,9 +8,9 @@ const Home = () => {
     <section class="bgimage">
     </section>
     <div class="flex-grid-thirds">
-  <div class="col">Headline 1</div>
-  <div class="col">Headline 2</div>
-  <div class="col">Headline 3</div>
+  <div class="col">Headline 1!</div>
+  <div class="col">Headline 2!</div>
+  <div class="col">Headline 3!</div>
 </div>
   </div>
   )
