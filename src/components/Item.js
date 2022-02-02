c39923e077e1f80888cac8227e452165d208b609
Dirk Knibbe <dirkknibbe@Dirks-MacBook-Pro.local>
2022-02-02 12:28:31 -0700

    displays items now yay

--- src/components/Item.js
+++ src/components/Item.js
@@ -2,25 +2,34 @@ import React from "react";
 import styled from "styled-components";
 
 const Item = (props) => {
-  const { items, handleDelete, handleEditSelect } = props;
+  const { item, handleDelete, handleEditSelect } = props;
 
   return (
-    <div>
-      <button
-        onClick={() => {
-          handleDelete(items.id);
-        }}
-      >
-        Delete
-      </button>
-      <button
-        onClick={() => {
-          handleEditSelect(items.id);
-        }}
-      >
-        Edit
-      </button>
-    </div>
+    <>
+      <div>
+        <h2>{item.item_name}</h2>
+        <p>Location: {item.item_location}</p>
+        <h4>{item.item_description}</h4>
+        <p>Price: ${item.item_price}</p>
+      </div>
+
+      {/* <div>
+        <button
+          onClick={() => {
+            handleDelete(item.item_id);
+          }}
+        >
+          Delete
+        </button>
+        <button
+          onClick={() => {
+            handleEditSelect(item.item_id);
+          }}
+        >
+          Edit
+        </button>
+      </div> */}
+    </>
   );
 };
 
