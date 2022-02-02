371d0500a2976f103e48bbc52928aae857cedf3c
Brandyn <stateofw@gmail.com>
2022-02-02 15:29:37 -0500

    homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,9 +8,9 @@ const Home = () => {
     <section className="bgimage">
     </section>
     <div className="flex-grid-thirds">
-  <div className="col">Headline 1!</div>
-  <div className="col">Headline 2!</div>
-  <div className="col">Headline 3!</div>
+  <div className="col"><p>A marketplace that allows for small business owners to promote their products</p></div>
+  <div className="col"><p>We empower small business owners, particularly women, to improve their business and economic opportunities to grow out of poverty.</p></div>
+  <div className="col"><p>Simple Easy and Flexible</p></div>
 </div>
   </div>
   )
